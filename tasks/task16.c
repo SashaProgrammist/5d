4ac630a2be7d55c3dfe44629ffdf56c7b0c5aa0b
SashaProgrammist <49712254+SashaProgrammist@users.noreply.github.com>
2022-02-22 21:19:45 +0300

    fix task 16

--- tasks/task16.c
+++ tasks/task16.c
@@ -1,16 +1,34 @@
 #include "..\libs\data_structures\matrix\matrix.h"
 #include <assert.h>
 
-int getCountOfSpecialItem(int *a, int n) {
-    int result = 0;
-    bool f = true;
+int getCountOfSpecialItem(const int *array, int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
 
-    for (int i = 1; i <= n; ++i)
-        if (i == n || a[i - 1] < a[i]) {
-            if (f) result++;
-            f = true;
-        } else
-            f = false;
+    int *minArray;
+    createIntArray(&minArray, n);
+    minArray[n - 1] = INT_MAX;
+    for (int i = n - 2; i >= 0; --i)
+        minArray[i] = min2(minArray[i + 1], array[i + 1]);
+
+    int *maxArray;
+    createIntArray(&maxArray, n);
+    maxArray[0] = -INT_MAX;
+    for (int i = 1; i < n; ++i)
+        maxArray[i] = max2(maxArray[i - 1], array[i - 1]);
+
+    int result = 0;
+
+    if (array[0] < minArray[0])result++;
+    if (array[n - 1] > maxArray[n - 1])result++;
+
+    for (int i = 1; i < n - 1; ++i)
+        if (array[i] < minArray[i] &&
+            array[i] > maxArray[i])
+            result++;
+
+    free((void *) minArray);
+    free((void *) maxArray);
 
     return result;
 }
@@ -29,19 +47,33 @@ void test_getCountOfSpecialItem_NoElement() {
 }
 
 void test_getCountOfSpecialItem_OneElement() {
-    assert(getCountOfSpecialItem((int[]) {1}, 1) == 1);
+    assert(getCountOfSpecialItem((int[]) {INT_MAX}, 1) == 1);
 }
 
 void test_getCountOfSpecialItem_SpecialItemOnEnd_notRecurringItem() {
-    assert(2 == getCountOfSpecialItem(
+    assert(0 == getCountOfSpecialItem(
             (int[]) {1, 2, 1, 0, -1, 0},
             6
     ));
 }
 
 void test_getCountOfSpecialItem_RecurringItem() {
-    assert(1 == getCountOfSpecialItem(
+    assert(0 == getCountOfSpecialItem(
             (int[]) {1, 1, 0, 0, 1, 3, -1, -1},
+            8
+    ));
+}
+
+void test_getCountOfSpecialItem_onSpecialItem() {
+    assert(3 == getCountOfSpecialItem(
+            (int[]) {1, 1, 3, 4, 5, 4, 7, 8},
+            8
+    ));
+}
+
+void test_getCountOfSpecialItem_ollSpecialItem() {
+    assert(8 == getCountOfSpecialItem(
+            (int[]) {1, 2, 3, 4, 5, 6, 7, 8},
             8
     ));
 }
@@ -51,15 +83,15 @@ void test_getCountOfSpecialItem() {
     test_getCountOfSpecialItem_OneElement();
     test_getCountOfSpecialItem_SpecialItemOnEnd_notRecurringItem();
     test_getCountOfSpecialItem_RecurringItem();
+    test_getCountOfSpecialItem_onSpecialItem();
+    test_getCountOfSpecialItem_ollSpecialItem();
 }
 
 void test_getCountOfSpecialItemInRows() {
     matrix m = createMatrixFromArray(
-            (int[]) {
-                    2, 2, 5, 5, 4,
-                    6, 2, 3, 8, 12,
-                    12, 12, 2, 1, 2
-            },
+            (int[]) { 2,  3,  5,  5,  4,
+                      6,  2,  3,  8, 12,
+                      12, 12,  2,  1,  2},
             3, 5
     );
 
